e1249ab1ccb5dfdbed6578e60cd9059ac8320824
Daffa Abiyyu <daffaabiyyu04@gmail.com>
2024-06-05 08:20:52 +0700

    bottom navigation bar

--- app/src/main/java/id/ac/astra/polytechnic/internak/MainActivity.java
+++ app/src/main/java/id/ac/astra/polytechnic/internak/MainActivity.java
@@ -1,41 +1,62 @@
 package id.ac.astra.polytechnic.internak;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import id.ac.astra.polytechnic.internak.databinding.ActivityMainBinding;
+import id.ac.astra.polytechnic.internak.ui.cage.CageFragment;
+import id.ac.astra.polytechnic.internak.ui.home.HomeFragment;
+import id.ac.astra.polytechnic.internak.ui.profile.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+        if (savedInstanceState == null) {
+            // Load the default fragment
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main, new HomeFragment())
+                    .commit();
+        }
 
-//        binding = ActivityMainBinding.inflate(getLayoutInflater());
-//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-//                R.id.navigation_home, R.id.nav_home, R.id.nav_cage, R.id.nav_profile)
-//                .build();
-//        NavController navController = Navigation.findNavController(this, R.id.bottomNavigationView);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+        binding.bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                if (item.getItemId() == R.id.nav_home) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.main, new HomeFragment())
+                            .commit();
+            } else if (item.getItemId() == R.id.nav_cage) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.main, new CageFragment())
+                            .commit();
+            } else if (item.getItemId() == R.id.nav_profile) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.main, new ProfileFragment())
+                            .commit();
+            }
+
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.main, selectedFragment)
+                            .commit();
+                }
+
+                return true;
+            }
+        });
     }
 }
